17480c5aa79222e5a848a45a685b06e43d9c4fe4
Guilherme <guisilva.207@gmail.com>
2022-02-03 09:16:04 -0300

    ProductCard e ProductCardDetails

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+
+function ShoppingCart(props) {
+  return (
+    <div>
+      <h1>Página de Carrinho</h1>
+      <p>{props.price}</p>
+    </div>
+  )
+}
+
+export default ShoppingCart
